2f24ca58197343ef0d186e1b4ef27931aeadadad
Toshiki TAKEUCHI <take@xcoo.jp>
2013-07-09 23:18:49 +0900

    A little refactoring

--- src/cljam/io.clj
+++ src/cljam/io.clj
@@ -1,113 +1,64 @@
 (ns cljam.io
   (:require [clojure.string :as str]
-            [clojure.java.io :only (file reader) :as io])
+            [clojure.java.io :only (file reader) :as io]
+            [cljam.sam :as sam])
   (:import (java.io DataOutputStream FileOutputStream)
-           (java.util.zip Deflater)))
-
-(defn- parse-header-keyvalues [keyvalues]
-  (map (fn [kv]
-         (let [[k v] (str/split kv #":")]
-           {(keyword k) v}))
-       keyvalues))
-
-(defn- parse-header-line [line]
-  (let [[type & keyvalues] (str/split line #"\t")]
-    {(keyword (subs type 1))
-     (vec (parse-header-keyvalues keyvalues))}))
-
-(defn- parse-optional-fields [options]
-  (map (fn [op]
-         (let [[tag type value] (str/split op #":")]
-           {(keyword tag) {:type type :value value}}))
-       options))
-
-(defn- parse-line [line]
-  (let [fields (str/split line #"\t")]
-    {:QNAME   (first fields)
-     :FLAG    (Integer/parseInt (nth fields 1))
-     :RNAME   (nth fields 2)
-     :POS     (Integer/parseInt (nth fields 3))
-     :MAPQ    (Integer/parseInt (nth fields 4))
-     :CIGAR   (nth fields 5)
-     :RNEXT   (nth fields 6)
-     :PNEXT   (Integer/parseInt (nth fields 7))
-     :TLEN    (Integer/parseInt (nth fields 8))
-     :SEQ     (nth fields 9)
-     :QUAL    (nth fields 10)
-     :options (vec (parse-optional-fields (drop 11 fields)))}))
-
-(defn- stringify-header-keyvalues [kv-vec]
-  (->> (map (fn [kv]
-              (let [[k v] (first (seq kv))]
-                (str (name k) \: v)))
-            kv-vec)
-       (str/join \tab)))
-
-(defn- stringify-header-map [amap]
-  (let [[type keyvalues] (first (seq amap))]
-    (str \@ (name type) \tab (stringify-header-keyvalues keyvalues))))
-
-(defn- stringify-optional-fields [options]
-  (->> (map (fn [op]
-              (let [[tag entity] (first (seq op))]
-                (str (name tag) \: (:type entity) \: (:value entity))))
-            options)
-       (str/join \tab)))
-
-(defn- stringify-alignment-map [amap]
-  (-> (str/join \tab [(:QNAME amap)
-                      (:FLAG  amap)
-                      (:RNAME amap)
-                      (:POS   amap)
-                      (:MAPQ  amap)
-                      (:CIGAR amap)
-                      (:RNEXT amap)
-                      (:PNEXT amap)
-                      (:TLEN  amap)
-                      (:SEQ   amap)
-                      (:QUAL  amap)
-                      (stringify-optional-fields (:options amap))])
-      str/trim))
+           net.sf.samtools.util.BlockCompressedOutputStream
+           (cljam.sam Sam SamHeader SamAlignment)))
 
 (defn slurp-sam
   "Opens a reader on sam-file and reads all its headers and alignments, returning a map about sam records."
   [sam-file]
   (with-open [r (io/reader sam-file)]
-    (loop [sam {:header [], :alignments []}, line (.readLine r)]
+    (loop [sam (Sam. [] [])
+           line (.readLine r)]
       (if (nil? line)
         sam
         (recur
          (if (= (first line) \@)
-           (assoc sam :header (conj (:header sam) (parse-header-line line)))
-           (assoc sam :alignments (conj (:alignments sam) (parse-line line))))
+           (assoc sam :header (conj (:header sam) (sam/parse-header line)))
+           (assoc sam :alignments (conj (:alignments sam) (sam/parse-alignment line))))
          (.readLine r))))))
 
 (defn spit-sam
   "Opposite of slurp-sam. Opens sam-file with writer, writes sam headers and alignments, then closes sam-file."
   [sam-file sam]
   (with-open [w (io/writer sam-file)]
-    (doseq [header-map (:header sam)]
-      (.write w (stringify-header-map header-map))
+    (doseq [sh (:header sam)]
+      (.write w (sam/stringify sh))
       (.newLine w))
-    (doseq [alignment-map (:alignments sam)]
-      (.write w (stringify-alignment-map alignment-map))
+    (doseq [sa (:alignments sam)]
+      (.write w (sam/stringify sa))
       (.newLine w))
     nil))
 
-(defn deflate [byte-data]
-  (let [ret (bytes nil)
-        defl (Deflater. 5 true)]
-    (.setInput defl byte-data)
-    (.finish defl)
-    (.deflate defl ret)
-    ret))
+(defn- stringify-header [headers]
+  (->> (map #(stringify-header-map %) headers)
+       (str/join \newline)))
 
-(defn spit-bam
-  [bam-file sam]
-  (with-open [w (DataOutputStream. (FileOutputStream. bam-file))]
-    (.write w (.getBytes "BAM\1"))
-    (doseq [header-map (:header sam)]
-      (.write w (.getBytes (stringify-header-map header-map))))
-    (doseq [alignment-map (:alignments sam)]
-      (.write w (.getBytes (stringify-alignment-map alignment-map))))
-    nil))
+(def fixed-block-size 32)
+
+;;; TODO
+(defn slurp-bam
+  "Opens a reader on bam-file and reads all its headers and alignments, returning a map about sam records."
+  [bam-file]
+  nil)
+
+;; (defn spit-bam
+;;   "Opposite of slurp-bam. Opens bam-file with writer, writes bam headers and alignments, then closes bam-file."
+;;   [bam-file sam]
+;;   (with-open [w (DataOutputStream. (BlockCompressedOutputStream. bam-file))]
+;;     ;; header
+;;     (.write w (.getBytes "BAM\1"))
+;;     (.write w (.getBytes (stringify-header (:header sam))))
+;;     (.write w (.getBytes "\n"))
+;;     ;; alignments
+;;     (doseq [alignment-map (:alignments sam)]
+;;       (.write w (+ fixed-block-size     ; TODO
+;;                    (count (:RNAME alignment-map))
+;;                    1
+;;                    (* cigar-length 4)
+;;                    (/ (+ read-length 1) 2)
+;;                    read-length))
+;;       (.write w ))
+;;     nil))
--- src/cljam/sam.clj
+++ src/cljam/sam.clj
@@ -1,3 +1,86 @@
 (ns cljam.sam
-  :use [clojure.java.io :only (file reader writer)]
-  :import [java.io.FileOutputStream :as fos])
+  (:require [clojure.string :as str]))
+
+(defrecord SamHeader [])
+
+(defrecord SamAlignment
+    [qname flag rname pos mapq cigar rnext pnext tlen seq qual options])
+
+(defrecord Sam
+    [header alignments])
+
+;;; parse
+
+(defn- parse-header-keyvalues [keyvalues]
+  (map (fn [kv]
+         (let [[k v] (str/split kv #":")]
+           {(keyword k) v}))
+       keyvalues))
+
+(defn parse-header [line]
+  "Parse a line, returning a SamHeader record."
+  (let [[type & keyvalues] (str/split line #"\t")]
+    (assoc (SamHeader.)
+      (keyword (subs type 1))
+      (vec (parse-header-keyvalues keyvalues)))))
+
+(defn- parse-optional-fields [options]
+  (map (fn [op]
+         (let [[tag type value] (str/split op #":")]
+           {(keyword tag) {:type type :value value}}))
+       options))
+
+(defn parse-alignment [line]
+  "Parse a line, returning a SamAlignment record."
+  (let [fields (str/split line #"\t")]
+    (SamAlignment. (first fields)
+                   (Integer/parseInt (nth fields 1))
+                   (nth fields 2)
+                   (Integer/parseInt (nth fields 3))
+                   (Integer/parseInt (nth fields 4))
+                   (nth fields 5)
+                   (nth fields 6)
+                   (Integer/parseInt (nth fields 7))
+                   (Integer/parseInt (nth fields 8))
+                   (nth fields 9)
+                   (nth fields 10)
+                   (vec (parse-optional-fields (drop 11 fields))))))
+
+;;; stringify
+
+(defn- stringify-header-keyvalues [kv-vec]
+  (->> (map (fn [kv]
+              (let [[k v] (first (seq kv))]
+                (str (name k) \: v)))
+            kv-vec)
+       (str/join \tab)))
+
+(defn- stringify-optional-fields [options]
+  (->> (map (fn [op]
+              (let [[tag entity] (first (seq op))]
+                (str (name tag) \: (:type entity) \: (:value entity))))
+            options)
+       (str/join \tab)))
+
+(defmulti stringify class)
+
+(defmethod stringify SamHeader
+  [sh]
+  (let [[type keyvalues] (first (seq sh))]
+    (str \@ (name type) \tab (stringify-header-keyvalues keyvalues))))
+
+(defmethod stringify SamAlignment
+  [sa]
+  (-> (str/join \tab [(:qname sa)
+                      (:flag  sa)
+                      (:rname sa)
+                      (:pos   sa)
+                      (:mapq  sa)
+                      (:cigar sa)
+                      (:rnext sa)
+                      (:pnext sa)
+                      (:tlen  sa)
+                      (:seq   sa)
+                      (:suql  sa)
+                      (stringify-optional-fields (:options sa))])
+      str/trim))

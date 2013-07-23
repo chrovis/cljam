fb9558278470d7a941623d318ef74626ecd52a51
Toshiki TAKEUCHI <take@xcoo.jp>
2013-07-23 14:35:41 +0900

    Temp. implementation of idxstats function using Picard

--- src/cljam/core.clj
+++ src/cljam/core.clj
@@ -7,7 +7,7 @@
             [cljam.bam :as bam]
             [cljam.sorter :as sorter]
             [cljam.indexer :as indexer])
-  (:import net.sf.picard.sam.BuildBamIndex))
+  (:import [net.sf.picard.sam BuildBamIndex BamIndexStats]))
 
 (defn view [& args]
   (with-command-line args
@@ -15,6 +15,9 @@
     [[header? "Include header in the output" false]
      [format "Specify input file format from <auto|sam|bam>" "auto"]
      files]
+    (when-not (= (count files) 1)
+      (println "Invalid arguments")
+      (System/exit 1))
     (let [asam (condp = format
                    "auto" (io/slurp     (first files))
                    "sam"  (io/slurp-sam (first files))
@@ -31,6 +34,9 @@
     [[src-format "Specify input file format from <auto|sam|bam>" "auto"]
      [dst-format "Specify output file format from <auto|sam|bam>" "auto"]
      files]
+    (when-not (= (count files) 2)
+      (println "Invalid arguments")
+      (System/exit 1))
     (let [asam (io/slurp (first files))]
       (io/spit (second files) asam))))
 
@@ -39,6 +45,9 @@
     "Usage: cljam sort [--order <coordinate|queryname>] <in.bam|sam> <out.bam|sam>"
     [[order "Specify sorting order of alignments from <coordinate|queryname>" "coordinate"]
      files]
+    (when-not (= (count files) 2)
+      (println "Invalid arguments")
+      (System/exit 1))
     (let [asam (io/slurp (first files))]
       (condp = order
         "coordinate" (io/spit (second files) (sorter/sort-by-pos asam))
@@ -48,19 +57,38 @@
   (with-command-line args
     "Usage: cljam index <in.bam>"
     [files]
+    (when-not (= (count files) 1)
+      (println "Invalid arguments")
+      (System/exit 1))
     ;; TODO: Should not use Picard
     (-> (BuildBamIndex.)
         (.instanceMain (into-array String [(str "I=" (first files)),
                                            (str "O=" (first files) ".bai")])))))
+
+(defn idxstats [& args]
+  (with-command-line args
+    "Usage: cljam idxstats <aln.bam>"
+    [files]
+    (when-not (= (count files) 1)
+      (println "Invalid arguments")
+      (System/exit 1))
+    ;; TODO: Should not use Picard
+    (-> (BamIndexStats.)
+        (.instanceMain (into-array String [(str "I=" (first files))])))))
+
+(defn merge [& args]
+  (with-command-line args
+    "Usage: cljam merge <in1.bam|sam> <in2.bam|sam> ... <out.bam|sam>"
+    [files]
+    (when (< (count files) 2)
+      (println "Invalid arguments")
+      (System/exit 1))))
 
 (defn pileup [& args]
   (with-command-line args
     "Usage: todo"
     [[foo "foo" 1]]
     (println "index " foo)))
-
-(defn idxstats [& args] "Usage :todo")
-(defn merge [& args] "Usage :todo")
 
 (defn -main [& args]
   (do-sub-command args
@@ -69,6 +97,6 @@
     [:convert "Convert SAM to BAM or BAM to SAM." convert]
     [:sort "Sort alignments by leftmost coordinates." sort]
     [:index "Index sorted alignment for fast random access. Index file <in.bam>.bai will be created." index]
-    [:idxstats "" idxstats]
-    [:merge "" merge]
+    [:idxstats "Retrieve  and print stats in the index file." idxstats]
+    [:merge "Merge multiple SAM/BAM." merge]
     [:pileup "" pileup]))

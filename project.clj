12120d90cf57b1c135fb23adf234c8b643a76d4a
Toshiki TAKEUCHI <take@xcoo.jp>
2013-08-06 23:52:09 +0900

    Modify to use release version `clj-sub-command`

--- project.clj
+++ project.clj
@@ -3,10 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories [["clojars classic" "http://clojars.org/repo/"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.utgenome.thirdparty/picard "1.86p"]
-                 [clj-sub-command "0.1.0-SNAPSHOT"]]
+                 [clj-sub-command "0.1.0"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
   :main cljam.core)

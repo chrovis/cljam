659e560364ee5ea99018a88f3665e0baaf0c47f3
Takashi AOKI <federkasten@me.com>
2013-09-15 16:59:19 +0900

    modify profile.clj to resolve dependencies via private nexus repositories

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["snapshots" {:url "https://nexus.xcoo.jp/content/repositories/snapshots"}]
+                 ["releases" {:url "https://nexus.xcoo.jp/content/repositories/releases"}]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.utgenome.thirdparty/picard "1.86p"]

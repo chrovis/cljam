13956461a793ab01ae62627dcf657d32eb9261b9
Toshiki TAKEUCHI <take@xcoo.jp>
2013-10-29 11:27:49 +0900

    Add doc comments to bgzf-util

--- src/clojure/cljam/util/bgzf_util.clj
+++ src/clojure/cljam/util/bgzf_util.clj
@@ -5,6 +5,7 @@
 (def ^:private address-mask 0xFFFFFFFFFFFF)
 
 (defn compare
+  "Negative if fp1 is earlier in file than fp2, positive if it is later, 0 if equal."
   [fp1 fp2]
   (cond
    (= fp1 fp2)                 0
@@ -14,10 +15,13 @@
    :else                       1))
 
 (defn get-block-address
+  "File offset of start of BGZF block for this file pointer."
   [fp]
   (bit-and (bit-shift-right fp shift-amount) address-mask))
 
 (defn same-or-adjacent-blocks?
+  "Returns true if fp2 points to somewhere in the same BGZF block, or the one
+  immediately following fp1's BGZF block."
   [fp1 fp2]
   (let [block1 (get-block-address fp1)
         block2 (get-block-address fp2)]

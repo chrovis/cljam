7e9abe7feae7075734ee82028d7045f415ca3f14
Toshiki TAKEUCHI <take@xcoo.jp>
2013-11-18 19:43:59 +0900

    Fix answers of pileup test

--- test/cljam/t_pileup.clj
+++ test/cljam/t_pileup.clj
@@ -4,8 +4,8 @@
   (:require [cljam.bam :as bam]
             [cljam.pileup :as plp]))
 
-(def test-bam-pileup-ref '(0 0 0 0 0 0 0 1 1 3 3 3 3 3 3 3 3 3 3 3 2 2 2 2 1 1 1 1 1 2 2 2 2 2 2 1 1 2 2 2 2 2 1 1 1 1))
-(def test-bam-pileup-ref2 '(0 1 2 2 2 2 3 3 3 3 4 4 5 5 6 6 6 6 6 6 6 6 5 5 4 4 4 4 4 3 3 3 3 3 3 3 2 1 0 0 0))
+(def test-bam-pileup-ref '(0 0 0 0 0 0 0 1 1 3 3 3 3 3 3 2 3 3 3 2 2 2 2 1 1 1 1 1 1 2 2 2 2 2 1 1 1 2 2 2 2 1 1 1 1 1))
+(def test-bam-pileup-ref2 '(0 1 2 2 2 2 3 3 3 3 4 4 5 5 6 6 6 6 6 6 6 5 5 4 4 4 4 4 3 3 3 3 3 3 3 2 1 0 0 0 0))
 
 (fact "pileup returns LazySeq"
       (type (plp/pileup (bam/reader test-sorted-bam-file) "ref")) => clojure.lang.LazySeq)

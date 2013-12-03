196ff8102db76464c760bc061a38b6bbba3a69b4
Takashi AOKI <federkasten@me.com>
2013-12-03 12:25:26 +0900

    remove :repositories option from project.clj

--- project.clj
+++ project.clj
@@ -3,8 +3,6 @@
   :url "https://chrov.is"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories [["snapshots" {:url "https://nexus.xcoo.jp/content/repositories/snapshots"}]
-                 ["releases" {:url "https://nexus.xcoo.jp/content/repositories/releases"}]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [org.clojure/tools.cli "0.2.4"]

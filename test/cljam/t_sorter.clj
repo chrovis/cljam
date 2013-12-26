24e2dc0b4a744de3f5d414de288f5ac33f3f7453
atsuo yamada <yamada-git@tir.jp>
2013-12-26 20:14:18 +0900

    add future tests for sort-by-qname

--- test/cljam/t_sorter.clj
+++ test/cljam/t_sorter.clj
@@ -18,9 +18,17 @@
                           (sam/writer tmp-coordinate-sorted-sam-file)) => nil
       (sorter/sort-by-pos (bam/reader test-bam-file)
                           (bam/writer tmp-coordinate-sorted-bam-file)) => nil
+      (sorter/sort-by-qname (sam/reader test-sam-file)
+                            (sam/writer tmp-queryname-sorted-sam-file)) =future=> nil
+      (sorter/sort-by-qname (bam/reader test-bam-file)
+                            (bam/writer tmp-queryname-sorted-bam-file)) =future=> nil
       (sorter/sorted-by? (sam/reader test-sam-file)) => falsey
       (sorter/sorted-by? (sam/reader tmp-coordinate-sorted-sam-file)) => truthy
       (sorter/sorted-by? (bam/reader tmp-coordinate-sorted-bam-file)) => truthy
+      (sorter/sorted-by? (sam/reader tmp-queryname-sorted-sam-file)) =future=> truthy
+      (sorter/sorted-by? (bam/reader tmp-queryname-sorted-bam-file)) =future=> truthy
       (sorter/sort-order (sam/reader test-sam-file)) => sorter/order-unknown
       (sorter/sort-order (sam/reader tmp-coordinate-sorted-sam-file)) => sorter/order-coordinate
-      (sorter/sort-order (bam/reader tmp-coordinate-sorted-bam-file)) => sorter/order-coordinate))
+      (sorter/sort-order (bam/reader tmp-coordinate-sorted-bam-file)) => sorter/order-coordinate
+      (sorter/sort-order (sam/reader tmp-queryname-sorted-sam-file)) =future=> sorter/order-queryname
+      (sorter/sort-order (bam/reader tmp-queryname-sorted-bam-file)) =future=> sorter/order-queryname))

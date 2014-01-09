162876c7e0e4a906b5b5bd84840e8a0da57e0e25
Takashi AOKI <federkasten@me.com>
2014-01-09 14:49:44 +0900

    remove sam/slurp, sam/spit, bam/slurp, and bam/spit #10

--- src/clojure/cljam/bam.clj
+++ src/clojure/cljam/bam.clj
@@ -11,28 +11,3 @@
 (defn writer
   [f]
   (bam-writer/writer f))
-
-(defn ^:deprecated
-  slurp
-  "Opens a reader on bam-file and reads all its headers and alignments,
-  returning a map about sam records."
-  [f & options]
-  (let [{:keys [chr start end] :or {chr nil
-                                    start 0
-                                    end -1}} options]
-    (with-open [r (bam-reader/reader f {})]
-      {:header (io/read-header r)
-       :alignments (io/read-alignments r
-                                       (if (nil? chr)
-                                         {}
-                                         {:chr chr :start start :end end}))})))
-
-(defn ^:deprecated
-  spit
-  "Opposite of slurp-bam. Opens bam-file with writer, writes sam headers and
-  alignments, then closes the bam-file."
-  [f sam]
-  (with-open [w (bam-writer/writer f)]
-    (io/write-header w (:header sam))
-    (io/write-refs w (:header sam))
-    (io/write-alignments w (:alignments sam) (:header sam))))
--- src/clojure/cljam/sam.clj
+++ src/clojure/cljam/sam.clj
@@ -11,21 +11,3 @@
 (defn writer
   [f]
   (sam-writer/writer f))
-
-(defn ^:deprecated
-  slurp
-  "Opens a reader on sam-file and reads all its headers and alignments,
-  returning a map about sam records."
-  [f]
-  (with-open [r (reader f)]
-    {:header (io/read-header r)
-     :alignments (vec (io/read-alignments r {}))}))
-
-(defn ^:deprecated
-  spit
-  "Opposite of slurp-sam. Opens sam-file with writer, writes sam headers and
-  alignments, then closes the sam-file."
-  [f sam]
-  (with-open [w (writer f)]
-    (io/write-header w (:header sam))
-    (io/write-alignments w (:alignments sam) nil)))

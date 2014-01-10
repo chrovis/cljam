de6b3614249bb62d75e8325efdd30ff1de5b7260
atsuo yamada <yamada-git@tir.jp>
2014-01-10 22:45:55 +0900

    add "medium.bam" for large file test (wip)

--- test/cljam/t_common.clj
+++ test/cljam/t_common.clj
@@ -161,3 +161,14 @@
               one)
             (filter #(= rname (:rname %)) (:alignments target-sam))))
         target-rnames))))
+
+(def medium-bam-file "test/resources/medium.bam")
+;;; How to generate medium.bam :
+;;; $ wget https://share.xcoo.jp/works/.../B6_all_bwa.sorted.bam
+;;; $ lein run convert B6_all_bwa.sorted.bam src.sam
+;;; $ grep '^@SQ' src.sam > r.head
+;;; $ grep -v '^@SQ' src.sam > r.body
+;;; $ cat r.body | perl -ne 'print $_ if rand() < 0.001' > r.body2
+;;; $ cat r.head r.body2 > result.sam
+;;; $ lein run convert result.sam test/resources/medium.bam
+

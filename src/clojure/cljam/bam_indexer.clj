3b7096078bfc1b3eaf9bf93fee04fe1dccf4fa0f
Toshiki TAKEUCHI <take@xcoo.jp>
2014-02-18 17:51:28 +0900

    Enable to read BAI

--- src/clojure/cljam/bam_indexer.clj
+++ src/clojure/cljam/bam_indexer.clj
@@ -1,7 +1,13 @@
 (ns cljam.bam-indexer
   (:require [cljam.bam :as bam]
             [cljam.io :as io]
+            [cljam.bam-indexer.reader :as bai-reader]
             [cljam.bam-indexer.writer :as bai-writer]))
+
+(defn read-index
+  [bai]
+  (with-open [r (bai-reader/reader bai)]
+    (bai-reader/read-index r)))
 
 (defn create-index
   [in-bam out-bai]
---
+++ src/clojure/cljam/bam_indexer/reader.clj
@@ -0,0 +1,61 @@
+(ns cljam.bam-indexer.reader
+  (:require [clojure.java.io :as io]
+            [cljam.lsb :as lsb]
+            [cljam.bam-indexer.common :refer [bai-magic]])
+  (:import java.util.Arrays
+           [java.io DataInputStream FileInputStream IOException]))
+
+(set! *warn-on-reflection* true)
+
+(deftype BAIReader [f ^DataInputStream reader]
+  java.io.Closeable
+  (close [this]
+    (.. this reader close)))
+
+(defn- read-chunks!
+  [^DataInputStream rdr len]
+  (loop [i 0, chunks []]
+    (if (< i len)
+      (recur (inc i) (conj chunks {:beg (lsb/read-long rdr)
+                                   :end (lsb/read-long rdr)}))
+      chunks)))
+
+(defn- read-bin-index!
+  [^DataInputStream rdr len]
+  (loop [i 0, index []]
+    (if (< i len)
+      (let [bin (lsb/read-int rdr)
+            n-chunk (lsb/read-int rdr)
+            chunks (read-chunks! rdr n-chunk)]
+        (recur (inc i) (conj index {:bin bin, :chunks chunks})))
+      index)))
+
+(defn- read-linear-index!
+  [^DataInputStream rdr len]
+  (loop [i 0, index []]
+    (if (< i len)
+      (recur (inc i) (conj index (lsb/read-long rdr)))
+      index)))
+
+(defn- read-index*!
+  [^DataInputStream rdr]
+  (when-not (Arrays/equals ^bytes (lsb/read-bytes rdr 4) (.getBytes ^String bai-magic))
+    (throw (IOException. "Invalid BAI file")))
+  (let [n-ref (lsb/read-int rdr)]
+    (loop [i 0, index []]
+      (if (< i n-ref)
+        (let [n-bin-index (lsb/read-int rdr)
+              new-bin-index (read-bin-index! rdr n-bin-index)
+              n-linear-index (lsb/read-int rdr)
+              new-linear-index (read-linear-index! rdr n-linear-index)]
+          (recur (inc i) (conj index {:bin-index new-bin-index
+                                      :linear-index new-linear-index})))
+        index))))
+
+(defn read-index
+  [^BAIReader rdr]
+  (read-index*! (.reader rdr)))
+
+(defn reader
+  [f]
+  (->BAIReader f (DataInputStream. (FileInputStream. (io/file f)))))

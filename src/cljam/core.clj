e474eea8d55a8c54e1bd89e475fa3ae6e768aedd
Toshiki TAKEUCHI <take@xcoo.jp>
2014-04-01 22:10:46 +0900

    Write docstrings
    Write docstrings:cljam.core, cljam.main, and cljam.cli

--- src/cljam/core.clj
+++ src/cljam/core.clj
@@ -1,14 +1,21 @@
 (ns cljam.core
+  "Core features of cljam."
   (:require [cljam.sam :as sam]
             [cljam.bam :as bam]))
 
-(defn reader [f & {:keys [ignore-index] :or {ignore-index true}}]
+(defn reader
+  "Selects suitable reader from f's extension, returning the reader. This
+  function supports SAM and BAM format."
+  [f & {:keys [ignore-index] :or {ignore-index true}}]
   (condp re-find f
     #"\.sam$" (sam/reader f)
     #"\.bam$" (bam/reader f :ignore-index ignore-index)
     (throw (IllegalArgumentException. "Invalid file type"))))
 
-(defn writer [f]
+(defn writer
+  "Selects suitable writer from f's extension, returning the writer. This
+  function supports SAM and BAM format."
+  [f]
   (condp re-find f
     #"\.sam$" (sam/writer f)
     #"\.bam$" (bam/writer f)

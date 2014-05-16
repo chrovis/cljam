4893cbea68b3d6110f5f48e6dd5383aac1945130
Toshiki TAKEUCHI <take@xcoo.jp>
2014-05-16 15:17:27 +0900

    Bump pandect version up

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/tools.logging "0.2.6"]
                  [org.clojure/tools.cli "0.3.1"]
                  [me.raynes/fs "1.4.5"]
-                 [pandect "0.3.1"]
+                 [pandect "0.3.2"]
                  [clj-sub-command "0.2.0"]
                  [bgzf4j "0.1.0"]]
   :plugins [[lein-midje "3.1.3"]

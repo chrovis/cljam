0ef85c52c514fa0c77ce19084b42bc5e70d20610
Takashi AOKI <federkasten@me.com>
2014-09-02 21:06:30 +0900

    improve normalization

--- src/cljam/util/chromosome.clj
+++ src/cljam/util/chromosome.clj
@@ -1,35 +1,38 @@
 (ns cljam.util.chromosome
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as cstr]))
 
 (defn normalize-name
   [s]
   (-> s
-      (str/replace #"\."  "_")
-      (str/replace #","  "_")
-      (str/replace #"\""  "")
-      (str/replace #"\'"  "")))
+      (cstr/replace #"\."  "_")
+      (cstr/replace #","  "_")
+      (cstr/replace #"\""  "")
+      (cstr/replace #"\'"  "")))
 
 (defn trim-chromosome-key
   [s]
   (-> s
-      (str/replace #"Chr" "")
-      (str/replace #"chr" "")))
+      (cstr/replace #"Chr" "")
+      (cstr/replace #"chr" "")))
 
 (defn- normalize-chromosome-prefix
   [s]
-  (let [m (re-matches #"(?i)^chr([0-9]+|X|Y|M|MT)" s)]
+  (let [m (re-matches #"(?i)^chr([0-9]{1,2}|X|Y|M|MT)(.*)$" s)]
     (if (nil? m)
       s
-      (let [[_ tail] m]
-        (str "chr" (str/upper-case tail))))))
+      (let [[_ base leftover] m
+            base  (if (re-find #"^\d+$" base)
+                    (str (Integer. base))
+                    base)]
+        (str "chr" (cstr/upper-case (str base leftover)))))))
 
 (defn- prepend-chromosome-prefix
   [s]
-  (let [m (re-matches #"(?i)^([0-9]+|X|Y|M|MT)" s)]
+  (let [m (re-matches #"(?i)^([0-9]{1,2}|X|Y|M|MT)(.*)$" s)]
     (if (nil? m)
       s
-      (let [[_ tail] m]
-        (str "chr" (str/upper-case tail))))))
+      (let [[_ base leftover] m]
+        (str "chr" (cstr/upper-case (str base leftover)))))))
 
 (defn normalize-chromosome-key
   [s]

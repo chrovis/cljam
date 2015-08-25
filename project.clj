cc5cec68d7d851001e8e8f7c555cbb1ea6fea6f7
Takashi AOKI <federkasten@me.com>
2015-08-25 00:22:39 -0700

    upgrade dependencies

--- project.clj
+++ project.clj
@@ -3,21 +3,21 @@
   :url "https://github.com/chrovis/cljam"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [org.clojure/tools.cli "0.3.1"]
-                 [me.raynes/fs "1.4.5"]
-                 [pandect "0.3.2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/tools.cli "0.3.3"]
+                 [me.raynes/fs "1.4.6"]
+                 [pandect "0.5.3"]
                  [clj-sub-command "0.2.1"]
                  [bgzf4j "0.1.0"]
                  [com.climate/claypoole "1.0.0"]]
   :plugins [[lein-midje "3.1.3"]
-            [lein-bin "0.3.4"]
-            [lein-marginalia "0.7.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]
+            [lein-bin "0.3.5"]
+            [lein-marginalia "0.8.0"]]
+  :profiles {:dev {:dependencies [[midje "1.7.0" :exclusions [slingshot]]
                                   [criterium "0.4.3"]
-                                  [cavia "0.1.3"]
-                                  [primitive-math "0.1.3"]]
+                                  [cavia "0.1.5"]
+                                  [primitive-math "0.1.4"]]
                    :global-vars {*warn-on-reflection* true}}}
   :main cljam.main
   :aot [cljam.main]

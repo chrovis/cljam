8df3a101f06accc909120797f46539796c552cd7
Jun Imura <imura@xcoo.jp>
2015-12-30 06:16:07 +0900

    fix for performance

--- src/cljam/level.clj
+++ src/cljam/level.clj
@@ -32,13 +32,13 @@
               (let [[next-state result] (f state (first s))]
                 (cons result (map-with-state f next-state (rest s)))))))
 
-(defn- calc-level [levels {:keys [pos seq] :as alignment}]
+(defn- calc-level
+  [levels {:keys [pos seq] :as alignment}]
   (let [segment [pos (+ pos (count seq))]
         level (or (some identity (map-indexed #(when-not (collide? segment %2) %1) levels))
                   (count levels))]
-    [(assoc levels level segment) ;; state
-     (update alignment :options conj {:LV {:type "i" :value level}}) ;; value
-     ]))
+    [(assoc levels level segment)
+     (update alignment :options conj {:LV {:type "i" :value level}})]))
 
 (defn- clean [path]
   (let [f (file path)]
@@ -56,24 +56,23 @@
             (throw (ex-info "Source BAM file must be sorted by coordinate."
                             {:type :bam-not-sorted})))
         ;; Split into per-chr BAM files.
-        caches (doall
-                (pmap
-                 #(let [{:keys [SN LN]} %
-                        cache-path (cache-name-fn SN)]
-                    (with-open [local-rdr (bam/reader (io/reader-path rdr) :ignore-index false)
-                                cache-wtr (bam/writer cache-path)]
-                      (io/write-header cache-wtr hdr)
-                      (io/write-refs cache-wtr hdr)
-                      (let [alignments (->> (io/read-alignments local-rdr {:chr SN :start 0 :end LN})
-                                            (map-with-state calc-level []))]
-                        (io/write-alignments cache-wtr alignments hdr)))
-                    cache-path)
-                 (hdr :SQ)))]
+        caches (pmap
+                #(let [{:keys [SN LN]} %
+                       cache-path (cache-name-fn SN)]
+                   (with-open [local-rdr (bam/reader (io/reader-path rdr) :ignore-index false)
+                               cache-wtr (bam/writer cache-path)]
+                     (io/write-header cache-wtr hdr)
+                     (io/write-refs cache-wtr hdr)
+                     (let [alignments (->> (io/read-alignments local-rdr {:chr SN :start 0 :end LN})
+                                           (map-with-state calc-level []))]
+                       (io/write-alignments cache-wtr alignments hdr)))
+                   cache-path)
+                (hdr :SQ))]
     ;; Merge cache files
     (io/write-header wtr hdr)
     (io/write-refs wtr hdr)
     (doseq [cache caches]
       (with-open [cache-rdr (bam/reader cache :ignore-index true)]
-        (io/write-alignments wtr (io/read-alignments cache-rdr) hdr))
+        (io/write-blocks wtr (io/read-blocks cache-rdr)))
       (clean cache))))
 

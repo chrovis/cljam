08a069a9a2177ec75ae64529096aa16c538275a5
Jun Imura <imura@xcoo.jp>
2016-01-06 13:41:45 +0900

    Modified level calculation.

--- src/cljam/level.clj
+++ src/cljam/level.clj
@@ -40,21 +40,29 @@
     [(assoc levels level segment)
      (update alignment :options conj {:LV {:type "i" :value level}})]))
 
-(defn- clean [path]
+(def ^:private chunk-size-alignment 10000)
+(def ^:private chunk-size-block 1500)
+
+(defn- add-level-st
+  [rdr wtr]
+  (let [hdr (io/read-header rdr)
+        aln (map-with-state calc-level [] (io/read-alignments rdr))]
+    (io/write-header wtr hdr)
+    (io/write-refs wtr hdr)
+    (io/write-alignments wtr aln hdr)))
+
+(defn- clean!
+  [path]
   (let [f (file path)]
     (when (.exists f)
       (.delete f))))
 
-(defmethod add-level :bam
+(defn- add-level-mt
   [rdr wtr]
   (let [filename (.getName (file (io/reader-path rdr)))
         basename (first (cstr/split filename #"\.(?=[^\.]+$)"))
         cache-name-fn #(format "%s/%s_%s.bam" util/temp-dir basename %)
         hdr (io/read-header rdr)
-        ;; Exit when not sorted.
-        _ (when-not (= (-> hdr :HD :SO) "coordinate")
-            (throw (ex-info "Source BAM file must be sorted by coordinate."
-                            {:type :bam-not-sorted})))
         ;; Split into per-chr BAM files.
         caches (pmap
                 #(let [{:keys [SN LN]} %
@@ -65,7 +73,8 @@
                      (io/write-refs cache-wtr hdr)
                      (let [alignments (->> (io/read-alignments local-rdr {:chr SN :start 0 :end LN})
                                            (map-with-state calc-level []))]
-                       (io/write-alignments cache-wtr alignments hdr)))
+                       (doseq [alignments-part (partition-all chunk-size-alignment alignments)]
+                         (io/write-alignments cache-wtr alignments-part hdr))))
                    cache-path)
                 (hdr :SQ))]
     ;; Merge cache files
@@ -73,6 +82,18 @@
     (io/write-refs wtr hdr)
     (doseq [cache caches]
       (with-open [cache-rdr (bam/reader cache :ignore-index true)]
-        (io/write-blocks wtr (io/read-blocks cache-rdr)))
-      (clean cache))))
+        (doseq [blk (partition-all chunk-size-block (io/read-blocks cache-rdr))]
+          (io/write-blocks wtr blk)))
+      (clean! cache))))
+
+(defn- check-bam-sorted!
+  [rdr]
+  (when-not (-> rdr io/read-header :HD :SO (= "coordinate"))
+    (throw (ex-info "Source BAM file must be sorted by coordinate."
+                    {:type :bam-not-sorted}))))
+
+(defmethod add-level :bam
+  [rdr wtr]
+  (check-bam-sorted! rdr)
+  (add-level-mt rdr wtr))
 

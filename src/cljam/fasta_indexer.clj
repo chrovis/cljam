a393127e2db6f08ad14cdb89a86076e683844772
Toshiki Takeuchi <take@xcoo.jp>
2017-03-03 18:01:58 +0900

    Improve performance of FASTA indexing

--- src/cljam/fasta_indexer.clj
+++ src/cljam/fasta_indexer.clj
@@ -1,11 +1,9 @@
 (ns cljam.fasta-indexer
   "Alpha - subject to change.
   Indexer of FASTA."
-  (:require [cljam.fasta :as fasta]
-            [cljam.fasta-index.core :as fai-core]))
+  (:require [cljam.fasta-index.core :as fai-core]))
 
 (defn create-index
   "Create a FASTA index file from the FASTA file."
   [in-fa out-fai]
-  (with-open [r (fasta/reader in-fa)]
-    (fai-core/create-index r out-fai)))
+  (fai-core/create-index in-fa out-fai))

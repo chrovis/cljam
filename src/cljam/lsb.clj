0298a6fa0cd1718694594533f4303513fe682c77
Jun Imura <imura@xcoo.jp>
2017-05-18 17:42:29 +0900

    Add a protocol to cljam.lsb.

--- src/cljam/lsb.clj
+++ src/cljam/lsb.clj
@@ -2,7 +2,7 @@
   "Reading/writing functions of stream and buffer for little-endian data."
   (:refer-clojure :exclude [read-string])
   (:require [cljam.util :refer [string->bytes bytes->string]])
-  (:import [java.io DataInputStream DataOutputStream EOFException RandomAccessFile InputStream]
+  (:import [java.io DataInput InputStream DataOutputStream EOFException ByteArrayOutputStream]
            [java.nio ByteBuffer ByteOrder]))
 
 (defn ^ByteBuffer gen-byte-buffer
@@ -13,141 +13,173 @@
   ([size]
      (.order (ByteBuffer/allocate size) ByteOrder/LITTLE_ENDIAN)))
 
-;; Skip
-;; ----
-
-(defprotocol Skippable
-  "Provides skipping data feature. Note that this feature is implemented by
-  protocol instead of multimethods for performance."
-  (skip [this n] "Skip over n bytes of data, discarding the skipped bytes."))
-
-(extend-protocol Skippable
-  InputStream
-  (skip [^DataInput rdr n]
-    (.skip rdr n))
-  ByteBuffer
-  (skip [^ByteBuffer bb n]
-    (.position bb (+ (.position bb) n)))
-  RandomAccessFile
-  (skip [^RandomAccessFile raf n]
-    (.skipBytes raf n)))
-
 ;; Reading
 ;; -------
 
-(defprotocol BytesReadble
-  "Provides bytes reading feature. Note that this feature is implemented by
-  Protocol instead of multimethods for performance."
-  (read-bytes [this len] [this buffer offset len]
-    "Reads up to len bytes of data."))
+(defprotocol LSBReadable
+  "Provides feature of reading little-endian values."
+  (skip [this length] "Skips over 'length' bytes of data, discarding the skipped bytes.")
+  (read-byte [this] "Reads 1 byte. Returns a byte value.")
+  (read-ubyte [this] "Reads 1 byte. Returns an unsigned byte value as long.")
+  (read-short [this] "Reads 2 bytes. Returns a short value.")
+  (read-ushort [this] "Reads 2 bytes. Returns an unsigned short value as long.")
+  (read-int [this] "Reads 4 bytes. Returns an int value.")
+  (read-uint [this] "Reads 4 bytes. Returns an unsigned int value as long.")
+  (read-long [this] "Reads 8 bytes. Returns a long value. ")
+  (read-float [this] "Reads 4 bytes. Returns a float value.")
+  (read-double [this] "Reads 8 bytes. Returns a double value.")
+  (read-bytes [this length] [this buffer offset length]
+    "Reads 'length' bytes to buffer starting from offset bytes. Returns a new byte-array if called without buffer.")
+  (read-string [this length] "Reads 'length' bytes. Returns a String.")
+  (read-null-terminated-string [this] "Reads until next null character. Returns a String without the null."))
 
-(extend-protocol BytesReadble
-  InputStream
+(extend-type ByteBuffer
+  LSBReadable
+  (skip [this ^long length]
+    (.position this (+ (.position this) length)))
+  (read-byte [this]
+    (.get this))
+  (read-ubyte [this]
+    (bit-and (.get this) 0xFF))
+  (read-short [this]
+    (.order this ByteOrder/LITTLE_ENDIAN)
+    (.getShort this))
+  (read-ushort [this]
+    (.order this ByteOrder/LITTLE_ENDIAN)
+    (bit-and (.getShort this) 0xFFFF))
+  (read-int [this]
+    (.order this ByteOrder/LITTLE_ENDIAN)
+    (.getInt this))
+  (read-uint [this]
+    (.order this ByteOrder/LITTLE_ENDIAN)
+    (bit-and (.getInt this) 0xFFFFFFFF))
+  (read-long [this]
+    (.order this ByteOrder/LITTLE_ENDIAN)
+    (.getLong this))
+  (read-float [this]
+    (.order this ByteOrder/LITTLE_ENDIAN)
+    (.getFloat this))
+  (read-double [this]
+    (.order this ByteOrder/LITTLE_ENDIAN)
+    (.getDouble this))
   (read-bytes
-    ([^InputStream rdr l]
-       (let [ba (byte-array l)]
-         (.read rdr ba 0 l)
-         ba))
-    ([^InputStream rdr buffer offset l]
-       (loop [total-read 0]
-         (when (< total-read l)
-           (let [n (.read rdr buffer (+ offset total-read) (- l total-read))]
-             (if (neg? n)
-               (throw (EOFException. "Premature EOF"))
-               (recur (+ total-read n))))))))
-  ByteBuffer
-  (read-bytes
-    ([^ByteBuffer bb len]
-       (let [ba (byte-array len)]
-         (.get bb ba 0 len)
-         ba))
-    ([^ByteBuffer bb buffer offset len]
-     (.get bb buffer offset len)))
-  RandomAccessFile
-  (read-bytes
-    ([^RandomAccessFile raf len]
-     (let [ba (byte-array len)]
-       (.readFully raf ba)
+    ([this ^long length]
+     (let [ba (byte-array length)]
+       (.get this ba)
        ba))
-    ([^RandomAccessFile raf buffer offset len]
-     (.readFully raf buffer offset len))))
+    ([this ^bytes buffer ^long offset ^long length]
+     (.get this buffer (int offset) (int length))
+     buffer))
+  (read-string [this ^long length]
+    (let [ba (byte-array length)]
+      (.get this ba)
+      (String. ba)))
+  (read-null-terminated-string [this]
+    (let [start (.position this)
+          end (do (while (not (zero? (.get this))))
+                  (.position this))
+          offset (.arrayOffset this)]
+      (String. (.array this) (+ offset start) (dec (- end start))))))
 
-(defn- read-byte-buffer
-  [rdr ^ByteBuffer bb l]
-  {:pre (<= l (.capacity bb))}
-  (read-bytes rdr (.array bb) 0 l)
-  (.limit bb (.capacity bb))
-  (.position bb l))
+(extend-type DataInput
+  LSBReadable
+  (skip [this ^long length]
+    (.skipBytes this length))
+  (read-byte [this]
+    (.readByte this))
+  (read-ubyte [this]
+    (.readUnsignedByte this))
+  (read-short [this]
+    (let [bb (gen-byte-buffer 2)]
+      (.readFully this (.array bb))
+      (.getShort bb)))
+  (read-ushort [this]
+    (bit-and (read-short this) 0xFFFF))
+  (read-int [this]
+    (let [bb (gen-byte-buffer 4)]
+      (.readFully this (.array bb))
+      (.getInt bb)))
+  (read-uint [this]
+    (bit-and (read-int this) 0xFFFFFFFF))
+  (read-long [this]
+    (let [bb (gen-byte-buffer 8)]
+      (.readFully this (.array bb))
+      (.getLong bb)))
+  (read-float [this]
+    (Float/intBitsToFloat (read-int this)))
+  (read-double [this]
+    (Double/longBitsToDouble (read-long this)))
+  (read-bytes
+    ([this ^long length]
+     (let [ba (byte-array length)]
+       (.readFully this ba)
+       ba))
+    ([this buffer ^long offset ^long length]
+     (.readFully this buffer offset length)
+     buffer))
+  (read-string [this ^long length]
+    (let [ba (byte-array length)]
+      (.readFully this ba)
+      (String. ba)))
+  (read-null-terminated-string [this]
+    (with-open [baos (ByteArrayOutputStream. 32)]
+      (loop []
+        (let [b (.readByte this)]
+          (when-not (zero? b)
+            (.write baos b)
+            (recur))))
+      (.toString baos))))
 
-(defn read-byte
-  [rdr]
-  (let [bb (gen-byte-buffer)]
-    (read-byte-buffer rdr bb 1)
-    (.flip bb)
-    (.get bb)))
-
-(defn read-ubyte
-  [rdr]
-  (let [bb (gen-byte-buffer)]
-    (read-byte-buffer rdr bb 1)
-    (.put bb (byte 0))
-    (.flip bb)
-    (.getShort bb)))
-
-(defn read-short
-  [rdr]
-  (let [bb (gen-byte-buffer)]
-    (read-byte-buffer rdr bb 2)
-    (.flip bb)
-    (.getShort bb)))
-
-(defn read-ushort
-  [rdr]
-  (let [bb (gen-byte-buffer)]
-    (read-byte-buffer rdr bb 2)
-    (.putShort bb (short 0))
-    (.flip bb)
-    (.getInt bb)))
-
-(defn read-int
-  [rdr]
-  (let [bb (gen-byte-buffer)]
-    (read-byte-buffer rdr bb 4)
-    (.flip bb)
-    (.getInt bb)))
-
-(defn read-uint
-  [rdr]
-  (let [bb (gen-byte-buffer)]
-    (read-byte-buffer rdr bb 4)
-    (.flip bb)
-    (bit-and 0xFFFFFFFF (.getInt bb))))
-
-(defn read-long
-  [rdr]
-  (let [bb (gen-byte-buffer)]
-    (read-byte-buffer rdr bb 8)
-    (.flip bb)
-    (.getLong bb)))
-
-(defn read-float
-  [rdr]
-  (let [bb (gen-byte-buffer)]
-    (read-byte-buffer rdr bb 4)
-    (.flip bb)
-    (.getFloat bb)))
-
-(defn read-string
-  [rdr ^Integer len]
-  (String. ^bytes (read-bytes rdr len) 0 0 len))
-
-(defn read-null-terminated-string
-  [^ByteBuffer bb]
-  (let [start (.position bb)
-        end (do (while (not (zero? (.get bb))))
-                (.position bb))
-        offset (.arrayOffset bb)]
-    (String. (.array bb) (+ offset start) (dec (- end start)))))
+(extend-type InputStream
+  LSBReadable
+  (skip [this ^long length]
+    (.skip this length))
+  (read-byte [this]
+    (unchecked-byte (.read this)))
+  (read-ubyte [this]
+    (bit-and (.read this) 0xFF))
+  (read-short [this]
+    (let [bb (gen-byte-buffer 2)]
+      (read-bytes this (.array bb) 0 2)
+      (.getShort bb)))
+  (read-ushort [this]
+    (bit-and (read-short this) 0xFFFF))
+  (read-int [this]
+    (let [bb (gen-byte-buffer 4)]
+      (read-bytes this (.array bb) 0 4)
+      (.getInt bb)))
+  (read-uint [this]
+    (bit-and (read-int this) 0xFFFFFFFF))
+  (read-long [this]
+    (let [bb (gen-byte-buffer 8)]
+      (read-bytes this (.array bb) 0 8)
+      (.getLong bb)))
+  (read-float [this]
+    (Float/intBitsToFloat (read-int this)))
+  (read-double [this]
+    (Double/longBitsToDouble (read-long this)))
+  (read-bytes
+    ([this ^long length]
+     (let [ba (byte-array length)]
+       (read-bytes this ba 0 length)))
+    ([this buffer ^long offset ^long length]
+     (loop [total-read 0]
+       (when (< total-read length)
+         (let [n (.read this buffer (+ offset total-read) (- length total-read))]
+           (if (neg? n)
+             (throw (EOFException. "Premature EOF"))
+             (recur (+ total-read n))))))
+     buffer))
+  (read-string [this ^long length]
+    (String. ^bytes (read-bytes this length)))
+  (read-null-terminated-string [this]
+    (with-open [baos (ByteArrayOutputStream. 32)]
+      (loop []
+        (let [b (.read this)]
+          (when-not (zero? b)
+            (.write baos b)
+            (recur))))
+      (.toString baos))))
 
 ;; Writing
 ;; -------

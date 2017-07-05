2274dd39be0c1e0bdc163dd40912d6dd5c8f7f2a
Toshiki Takeuchi <take@xcoo.jp>
2017-07-05 13:02:19 +0900

    Support chromosome normalization of chrUn

--- src/cljam/util/chromosome.clj
+++ src/cljam/util/chromosome.clj
@@ -15,16 +15,17 @@
 
 (defn- normalize-chromosome-prefix
   [s]
-  (if-let [[_ base leftover] (re-matches #"(?i)chr([0-9]{1,2}|X|Y|M|MT)(.*)" s)]
-    (let [base (if (re-matches #"\d+" base)
-                 (str (Integer/parseInt base))
-                 base)]
-      (str "chr" (cstr/upper-case (str base leftover))))
+  (if-let [[_ base leftover] (re-matches #"(?i)chr([0-9]{1,2}|X|Y|M|MT|Un)(.*)" s)]
+    (let [base* (condp re-matches base
+                  #"\d+" (str (Integer/parseInt base))
+                  #"(?i)Un" "Un"
+                  (cstr/upper-case base))]
+      (str "chr" (str base* (cstr/upper-case leftover))))
     s))
 
 (defn- prepend-chromosome-prefix
   [s]
-  (if (re-matches #"(?i)([0-9]{1,2}|X|Y|M|MT).*" s)
+  (if (re-matches #"(?i)([0-9]{1,2}|X|Y|M|MT|Un).*" s)
     (str "chr" s)
     s))
 

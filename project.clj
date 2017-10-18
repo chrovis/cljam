26810add970ddb392a45b990913b4131965d8568
alumi <alumi@users.noreply.github.com>
2017-10-18 16:50:55 +0900

    Upgrade clojure to 1.9.0-beta2.

--- project.clj
+++ project.clj
@@ -29,7 +29,7 @@
                    :global-vars {*warn-on-reflection* true}}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha19"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-beta2"]]}
              :uberjar {:dependencies [[org.clojure/clojure "1.8.0"]
                                       [org.apache.logging.log4j/log4j-api "2.9.0"]
                                       [org.apache.logging.log4j/log4j-core "2.9.0"]]

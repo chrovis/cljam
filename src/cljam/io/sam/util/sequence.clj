78003eb3fbd02ebabe3906e501bed531aa0377c4
alumi <alumi@users.noreply.github.com>
2017-12-21 16:27:06 +0900

    Add a function to compute reverse complement sequence.

--- src/cljam/io/sam/util/sequence.clj
+++ src/cljam/io/sam/util/sequence.clj
@@ -74,3 +74,28 @@
         (= b (byte \.)) (aset-byte bases i (byte \N))
         (<= (byte \a) b (byte \z)) (aset-byte bases i (- b 32))))) ;; Upper-case ASCII offset
   bases)
+
+(def ^:private revcomp-table
+  (let [ba (byte-array 128)]
+    (dotimes [i 128] (aset-byte ba i (byte \N)))
+    (doseq [s ["AT" "GC" "NN"]
+            [a b] [(cstr/upper-case s) (cstr/lower-case s)]]
+      (aset-byte ba (int a) (byte b))
+      (aset-byte ba (int b) (byte a)))
+    ba))
+
+(defn reverse-complement
+  "Returns a reverse-complement sequence of given sequence. All characters other
+  than ATGCNatgcn will be replaced with N. Masks will be kept."
+  [^String s]
+  (let [l (.length s)
+        cb (CharBuffer/allocate l)]
+    (dotimes [i l]
+      (->> (unchecked-dec (- l i))
+           (.charAt s)
+           unchecked-int
+           (aget ^bytes revcomp-table)
+           unchecked-char
+           (.put cb)))
+    (.flip cb)
+    (.toString cb)))

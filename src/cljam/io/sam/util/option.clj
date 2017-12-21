bb0e7de6e621ca332cebb501d56908bdbca6559d
alumi <alumi@users.noreply.github.com>
2017-12-21 19:09:56 +0900

    Add parsers for optional fields of SAM.

--- src/cljam/io/sam/util/option.clj
+++ src/cljam/io/sam/util/option.clj
@@ -17,7 +17,10 @@
     \C (p/as-long val)
     \f (p/as-double val)
     \H (p/hex->bytes val)
-    (throw (Exception. (format "Unrecognized tag type: %s, for value: %s" val-type val)))))
+    (-> "Unrecognized tag type: %s, for value: %s"
+        (format val-type val)
+        Exception.
+        throw)))
 
 (defn parse-optional-field [op]
   (let [[tag val-type-str val] (cstr/split op #":" 3)
@@ -36,3 +39,114 @@
           (let [[tag {:keys [type value]}] (first (seq op))]
             (cstr/join \: [(name tag) type value]))))
        (cstr/join \tab)))
+
+;;; accessors
+
+(defn value-for-tag
+  [tag aln]
+  (:value (some tag (:options aln))))
+
+(def
+  ^{:doc "CIGAR string of the mate alignment."
+    :arglists '([aln])}
+  mate-cigar
+  (partial value-for-tag :MC))
+
+(def
+  ^{:doc "Score of the alignment."
+    :arglists '([aln])}
+  score
+  (partial value-for-tag :AS))
+
+(def
+  ^{:doc "Barcode sequence."
+    :arglists '([aln])}
+  barcode
+  (partial value-for-tag :BC))
+
+(def
+  ^{:doc "Edit distance from reference of the alignment."
+    :arglists '([aln])}
+  edit-distance
+  (partial value-for-tag :NM))
+
+(defn parse-mismatching-positions-str
+  "Parse mismatching positions in the SAM optional fields. Returns a sequence
+  consisting of vectors which are one of [:match matching-length],
+  [:mismatch a-reference-base-char] and [:deletion reference-bases-string]."
+  [s]
+  (when s
+    (let [[_ head tail] (re-matches #"(\d+)(.*)" s)]
+      (->> (re-seq #"(([A-Z])|\^([A-Z]+))(\d+)" tail)
+           (mapcat
+            (fn [[_ _ mismatch deletion match]]
+              [(if mismatch
+                 [:mismatch (first mismatch)]
+                 [:deletion deletion])
+               [:match (p/as-long match)]]))
+           (cons [:match (p/as-long head)])))))
+
+(def
+  ^{:doc "Mismatching positions and bases of the alignment."
+    :arglists '([aln])}
+  mismatching-positions
+  (comp parse-mismatching-positions-str (partial value-for-tag :MD)))
+
+(def
+  ^{:doc "Ratio of the primary alignment score and the alternative one."
+    :arglists '([aln])}
+  primary-to-alt-score
+  (partial value-for-tag :pa))
+
+(defn parse-supplementary-alignments-str
+  "Parse serialized supplementary alignments."
+  [s]
+  (when s
+    (->> (re-seq #"(\S+?),(\d+),([+-]),((?:\d+[MIDSH])+),(\d+),(\d+);" s)
+         (map (fn [[_ & rests]]
+                (-> [:rname :pos :strand :cigar :mapq :edit-distance]
+                    (zipmap rests)
+                    (update :pos p/as-long)
+                    (update :mapq p/as-long)
+                    (update :strand first)
+                    (update :edit-distance p/as-long)))))))
+
+(def
+  ^{:doc "List of supplementary alignments."
+    :arglists '([aln])}
+  supplementary-alignments
+  (comp parse-supplementary-alignments-str (partial value-for-tag :SA)))
+
+(defn parse-alternative-hits-str
+  "Parse serialized supplementary alignments."
+  [s]
+  (when s
+    (->> (re-seq #"(\S+?),([+-]?\d+),((?:\d+[MIDSH])+),(\d+);" s)
+         (map (fn [[_ & rests]]
+                (-> (zipmap [:rname :pos :cigar :edit-distance] rests)
+                    (update :pos p/as-long)
+                    (update :edit-distance p/as-long)))))))
+
+(def
+  ^{:doc "List of alternative alignments."
+    :arglists '([aln])}
+  alternative-hits
+  (comp parse-alternative-hits-str (partial value-for-tag :XA)))
+
+(def
+  ^{:doc "Suboptimal alignment score."
+    :arglists '([aln])}
+  suboptimal-score
+  (partial value-for-tag :XS))
+
+(def
+  ^{:doc "Name of read group of the alignment."
+    :arglists '([aln])}
+  read-group
+  (partial value-for-tag :RG))
+
+(def
+  ^{:doc "Comment of reference sequence."
+    :arglists '([aln])}
+  ref-comment
+  (partial value-for-tag :XR))
---
+++ test/cljam/io/sam/util/option_test.clj
@@ -0,0 +1,68 @@
+(ns cljam.io.sam.util.option-test
+  (:require [clojure.test :refer :all]
+            [cljam.io.sam.util.option :as opt]))
+
+(deftest parse-optional-field
+  (testing "regression"
+    (are [?str ?expect]
+        (and (= (opt/parse-optional-field ?str) ?expect)
+             (= (opt/stringify-optional-fields [?expect]) ?str))
+      "AS:i:0" {:AS {:type "i" :value 0}}
+      "BC:Z:CGTAC" {:BC {:type "Z" :value "CGTAC"}}
+      "pa:f:0.987" {:pa {:type "f" :value 0.987}}
+      "XX:A:A" {:XX {:type "A" :value \A}}
+      "ZZ:B:i,1,2,3" {:ZZ {:type "B" :value "i,1,2,3"}}))
+  (testing "hex"
+    (is (= (update-in (opt/parse-optional-field "YY:H:1AE301") [:YY :value] seq)
+           {:YY {:type "H" :value (map unchecked-byte [0x1A 0xE3 0x1])}}))))
+
+(deftest stringify-optional-fields
+  (is (= (opt/stringify-optional-fields [])
+         ""))
+  (is (= (opt/stringify-optional-fields [{:AS {:type "i" :value 0}} {:BC {:type "Z" :value "AAT"}}])
+         "AS:i:0\tBC:Z:AAT")))
+
+(deftest optional-fields
+  (let [s ["NM:i:4"
+           "MD:Z:103"
+           "MC:Z:2S13M"
+           "AS:i:103"
+           "XS:i:103"
+           "RG:Z:ReadGroup01"
+           "BC:Z:CGTAC"
+           "SA:Z:chr1,550,+,70S50M,0,4;chr1,550,-,40S30M50S,0,0;"
+           "pa:f:0.987"
+           "XA:Z:chr18,-80000000,10S90M10D20M10D10M,6;chr1,-240000000,10M10D70M10I10M10I10M10I10M,7;"
+           "XR:Z:Reference Comment"]
+        aln {:options (map opt/parse-optional-field s)}]
+    (is (= (opt/score aln) 103))
+    (is (= (opt/edit-distance aln) 4))
+    (is (= (opt/mismatching-positions aln) [[:match 103]]))
+    (is (= (opt/mate-cigar aln) "2S13M"))
+    (is (= (opt/suboptimal-score aln) 103))
+    (is (= (opt/read-group aln) "ReadGroup01"))
+    (is (= (opt/barcode aln) "CGTAC"))
+    (is (= (opt/supplementary-alignments aln)
+           [{:rname "chr1", :pos 550, :strand \+,
+             :cigar "70S50M", :mapq 0, :edit-distance 4}
+            {:rname "chr1", :pos 550, :strand \-,
+             :cigar "40S30M50S", :mapq 0, :edit-distance 0}]))
+    (is (= (opt/primary-to-alt-score aln) 0.987))
+    (is (= (opt/alternative-hits aln)
+           [{:rname "chr18", :pos -80000000, :cigar "10S90M10D20M10D10M", :edit-distance 6}
+            {:rname "chr1", :pos -240000000, :cigar "10M10D70M10I10M10I10M10I10M", :edit-distance 7}]))
+    (is (= (opt/ref-comment aln) "Reference Comment"))))
+
+(deftest md-string
+  (are [?str ?expect]
+      (= (opt/parse-mismatching-positions-str ?str) ?expect)
+    "100"
+    [[:match 100]]
+    "132T0"
+    [[:match 132] [:mismatch \T] [:match 0]]
+    "10A5^AC6"
+    [[:match 10] [:mismatch \A] [:match 5] [:deletion "AC"] [:match 6]]
+    "30C5C35C20"
+    [[:match 30] [:mismatch \C] [:match 5] [:mismatch \C] [:match 35] [:mismatch \C] [:match 20]]
+    "55^CTAACCCTAA63T8A2"
+    [[:match 55] [:deletion "CTAACCCTAA"] [:match 63] [:mismatch \T] [:match 8] [:mismatch \A] [:match 2]]))

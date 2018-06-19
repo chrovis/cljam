7be498cb2f8b874e326a592e6b30bc7caf4c82bc
alumi <alumi@users.noreply.github.com>
2018-06-19 14:33:37 +0900

    Upgrade lein-binplus to 0.6.4

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
                                   [net.totakke/libra "0.1.1"]
                                   [org.tcrawley/dynapath "1.0.0"]
                                   [se.haleby/stub-http "0.2.5"]]
-                   :plugins [[lein-binplus "0.6.2" :exclusions [org.clojure/clojure]]
+                   :plugins [[lein-binplus "0.6.4" :exclusions [org.clojure/clojure]]
                              [lein-codox "0.10.3"]
                              [lein-marginalia "0.9.1" :exclusions [org.clojure/clojure]]
                              [lein-cloverage "1.0.10" :exclusions [org.clojure/clojure org.tcrawley/dynapath]]

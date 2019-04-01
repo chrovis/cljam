dcb257bfb780249e52118f57e322161107793a23
alumi <alumi@users.noreply.github.com>
2019-04-01 16:38:48 +0900

    Update lein-binplus to 0.6.5

--- project.clj
+++ project.clj
@@ -17,7 +17,7 @@
                                   [criterium "0.4.4"]
                                   [net.totakke/libra "0.1.1"]
                                   [se.haleby/stub-http "0.2.7"]]
-                   :plugins [[lein-binplus "0.6.4" :exclusions [org.clojure/clojure]]
+                   :plugins [[lein-binplus "0.6.5" :exclusions [org.clojure/clojure]]
                              [lein-codox "0.10.5"]
                              [lein-marginalia "0.9.1" :exclusions [org.clojure/clojure]]
                              [lein-cloverage "1.0.13"]

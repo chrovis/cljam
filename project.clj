211fe8e386be6839d92707b14f0157008c532077
alumi <alumi@users.noreply.github.com>
2019-12-16 15:56:21 +0900

    Update log4j to 2.13.0

--- project.clj
+++ project.clj
@@ -33,8 +33,8 @@
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.10 {:dependencies [[org.clojure/clojure "1.10.1"]]}
              :uberjar {:dependencies [[org.clojure/clojure "1.10.1"]
-                                      [org.apache.logging.log4j/log4j-api "2.12.1"]
-                                      [org.apache.logging.log4j/log4j-core "2.12.1"]]
+                                      [org.apache.logging.log4j/log4j-api "2.13.0"]
+                                      [org.apache.logging.log4j/log4j-core "2.13.0"]]
                        :resource-paths ["bin-resources"]
                        :main cljam.tools.main
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]

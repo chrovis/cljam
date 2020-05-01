c479aaef77345d58915cbf31a0d40840b59e9d31
alumi <alumi@users.noreply.github.com>
2020-05-01 10:13:35 +0900

    Update tools.cli to 1.0.194

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/core.memoize "1.0.236"]
                  [org.clojure/tools.logging "1.1.0"]
-                 [org.clojure/tools.cli "0.4.2"]
+                 [org.clojure/tools.cli "1.0.194"]
                  [org.apache.commons/commons-compress "1.20"]
                  [clj-sub-command "0.6.0"]
                  [digest "1.4.9"]

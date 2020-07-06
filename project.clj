4fbbbdbe7f1f0ea8de52e846ffc042155d966d35
alumi <alumi@users.noreply.github.com>
2020-07-06 16:36:58 +0900

    Update criterium to 0.4.6

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                  [proton "0.1.8"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
                                   [cavia "0.5.1"]
-                                  [criterium "0.4.5"]
+                                  [criterium "0.4.6"]
                                   [net.totakke/libra "0.1.1"]
                                   [se.haleby/stub-http "0.2.8"]]
                    :plugins [[lein-binplus "0.6.6" :exclusions [org.clojure/clojure]]

650998589eb3039d12fe3b5f02618d50ee655028
alumi <alumi@users.noreply.github.com>
2021-09-28 16:57:28 +0900

    Update org.apache.commons/commons-compress to 1.21

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/core.memoize "1.0.250"]
                  [org.clojure/tools.logging "1.1.0"]
                  [org.clojure/tools.cli "1.0.206"]
-                 [org.apache.commons/commons-compress "1.20"]
+                 [org.apache.commons/commons-compress "1.21"]
                  [clj-sub-command "0.6.0"]
                  [digest "1.4.9"]
                  [bgzf4j "0.1.0"]

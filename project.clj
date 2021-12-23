bc0b6f4bdac75c515bef9492fe3560b88dc468c5
alumi <alumi@users.noreply.github.com>
2021-12-23 17:52:45 +0900

    Update org.clojure/tools.logging to 1.2.3

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/core.memoize "1.0.253"]
-                 [org.clojure/tools.logging "1.1.0"]
+                 [org.clojure/tools.logging "1.2.3"]
                  [org.clojure/tools.cli "1.0.206"]
                  [org.apache.commons/commons-compress "1.21"]
                  [clj-sub-command "0.6.0"]

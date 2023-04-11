78338823b4185b537612fc14f1c8c95bd9751b6b
alumi <alumi@users.noreply.github.com>
2023-04-11 17:39:30 +0900

    Update camel-snake-kebab to 0.4.3

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  [digest "1.4.10"]
                  [bgzf4j "0.1.0"]
                  [com.climate/claypoole "1.1.4"]
-                 [camel-snake-kebab "0.4.2"]
+                 [camel-snake-kebab "0.4.3"]
                  [proton "0.2.2"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.3"]
                                   [cavia "0.5.1"]

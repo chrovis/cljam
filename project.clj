02f2cdd3285cd8646688f2c05974f2648102ae2e
alumi <alumi@users.noreply.github.com>
2023-04-11 17:42:22 +0900

    Update org.clojure/tools.cli to 1.0.214

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/core.memoize "1.0.257"]
                  [org.clojure/tools.logging "1.2.4"]
-                 [org.clojure/tools.cli "1.0.206"]
+                 [org.clojure/tools.cli "1.0.214"]
                  [org.apache.commons/commons-compress "1.21"]
                  [clj-sub-command "0.6.0"]
                  [digest "1.4.10"]

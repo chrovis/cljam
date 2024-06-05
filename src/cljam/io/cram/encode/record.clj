0e0352aed76d5c004544ab00dec5e57354cea9f4
Shogo Ohta <shogo@xcoo.jp>
2024-06-05 11:12:09 +0900

    [ci skip] wip: stats

--- src/cljam/io/cram/encode/record.clj
+++ src/cljam/io/cram/encode/record.clj
@@ -2,22 +2,42 @@
   (:require [cljam.io.sam.util.flag :as sam.flag]
             [cljam.io.sam.util.option :as sam.option]))
 
+(defn- make-alignment-stats [^long nrefs]
+  (let [refs (boolean-array (inc nrefs))
+        stats (long-array 4)]
+    (fn
+      ([]
+       (let [referred (into #{} (keep-indexed (fn [i r] (when r i))) refs)]
+         {:ref-seq-id (cond (> (count referred) 1) -2
+                            (contains? referred nrefs) -1
+                            :else (or (first referred) -1))
+          :start (aget stats 0)
+          :span (- (aget stats 1) (aget stats 0))
+          :bases (aget stats 2)
+          :records (aget stats 3)}))
+      ([{^long ri ::ref-index ^long end ::end :keys [^long pos] :as record}]
+       (let [idx (if (neg? ri) nrefs ri)]
+         (aset refs idx true)
+         (when (pos? pos)
+           (when (< pos (aget stats 0))
+             (aset stats 0 pos))
+           (when (< (aget stats 1) end)
+             (aset stats 1 end)))
+         (aset stats 2 (+ (aget stats 2) (long (count (:seq record)))))
+         (aset stats 3 (inc (aget stats 3))))))))
+
 (defn- ref-index [rname->idx rname]
   (if  (= rname "*")
     -1
     (get rname->idx rname)))
 
 (defn- build-positional-data-encoder [cram-header {:keys [RI RL AP RG]}]
-  (let [rname->idx (into {}
-                         (map-indexed (fn [i {:keys [SN]}] [SN i]))
-                         (:SQ cram-header))
-        rg-id->idx (into {}
+  (let [rg-id->idx (into {}
                          (map-indexed (fn [i {:keys [ID]}] [ID i]))
                          (:RG cram-header))]
     (fn [record]
-      (let [ri (ref-index rname->idx (:rname record))
-            rg (sam.option/value-for-tag :RG record)]
-        (RI ri)
+      (let [rg (sam.option/value-for-tag :RG record)]
+        (RI (::ref-index record))
         (RL (count (:seq record)))
         (AP (:pos record))
         (RG (if rg (get rg-id->idx rg) -1))))))
@@ -26,21 +46,20 @@
   (fn [record]
     (RN (.getBytes ^String (:qname record)))))
 
-(defn- build-mate-read-encoder [cram-header {:keys [MF NS NP TS]}]
-  (let [rname->idx (into {}
-                         (map-indexed (fn [i {:keys [SN]}] [SN i]))
-                         (:SQ cram-header))]
-    (fn [{:keys [^long flag rname rnext] :as record}]
-      (let [mate-flag (cond-> 0
-                        (pos? (bit-and flag (sam.flag/encoded #{:next-reversed})))
-                        (bit-or 0x01)
+(defn- build-mate-read-encoder [rname->idx {:keys [MF NS NP TS]}]
+  (fn [{:keys [^long flag rnext] :as record}]
+    (let [mate-flag (cond-> 0
+                      (pos? (bit-and flag (sam.flag/encoded #{:next-reversed})))
+                      (bit-or 0x01)
 
-                        (pos? (bit-and flag (sam.flag/encoded #{:next-unmapped})))
-                        (bit-or 0x02))]
-        (MF mate-flag)
-        (NS (ref-index rname->idx (if (= rnext "=") rname rnext)))
-        (NP (:pnext record))
-        (TS (:tlen record))))))
+                      (pos? (bit-and flag (sam.flag/encoded #{:next-unmapped})))
+                      (bit-or 0x02))]
+      (MF mate-flag)
+      (NS (if (= rnext "=")
+            (::ref-index record)
+            (ref-index rname->idx rnext)))
+      (NP (:pnext record))
+      (TS (:tlen record)))))
 
 (defn- build-auxiliary-tags-encoder [{:keys [TL]} _tag-encoders]
   (fn [_record]
@@ -73,14 +92,17 @@
       (encode-qual record QS))))
 
 (defn- build-cram-record-encoder
-  [cram-header {:keys [BF CF] :as ds-encoders} tag-encoders]
-  (let [pos-encoder (build-positional-data-encoder cram-header ds-encoders)
+  [cram-header {:keys [BF CF] :as ds-encoders} tag-encoders stats]
+  (let [rname->idx (into {}
+                         (map-indexed (fn [i {:keys [SN]}] [SN i]))
+                         (:SQ cram-header))
+        pos-encoder (build-positional-data-encoder cram-header ds-encoders)
         name-encoder (build-read-name-encoder ds-encoders)
-        mate-encoder (build-mate-read-encoder cram-header ds-encoders)
+        mate-encoder (build-mate-read-encoder rname->idx ds-encoders)
         tags-encoder (build-auxiliary-tags-encoder ds-encoders tag-encoders)
         mapped-encoder (build-mapped-read-encoder ds-encoders)
         unmapped-encoder (build-unmapped-read-encoder ds-encoders)]
-    (fn [record]
+    (fn [{:keys [^long pos] :as record}]
       (let [bf (bit-and (long (:flag record))
                         (bit-not (sam.flag/encoded #{:next-reversed :next-unmapped})))
             cf (cond-> 0
@@ -88,7 +110,14 @@
                  true (bit-or 0x02)
                  false (bit-or 0x04)
                  (= (:seq record) "*") (bit-or 0x08))
-            record' (assoc record ::flag cf)]
+            record' (assoc record
+                           ::flag cf
+                           ::ref-index (ref-index rname->idx (:rname record))
+                           ::end (if (pos? pos)
+                                   ;; TODO: need to be calculated from read features
+                                   (+ pos (count (:seq record)))
+                                   0))]
+        (stats record')
         (BF bf)
         (CF cf)
         (pos-encoder record')
@@ -102,5 +131,7 @@
 (defn encode-slice-records
   "TODO"
   [cram-header ds-encoders tag-encoders records]
-  (let [record-encoder (build-cram-record-encoder cram-header ds-encoders tag-encoders)]
-    (run! record-encoder records)))
+  (let [stats (make-alignment-stats (count (:SQ cram-header)))
+        record-encoder (build-cram-record-encoder cram-header ds-encoders tag-encoders stats)]
+    (run! record-encoder records)
+    (stats)))

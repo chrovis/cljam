6781f658036260ac309c349671e8a93db86761d1
Shogo Ohta <shogo@xcoo.jp>
2024-06-11 11:00:36 +0900

    [ci skip] wip: divide alignments into multiple slices/containers

--- src/cljam/io/cram/writer.clj
+++ src/cljam/io/cram/writer.clj
@@ -43,13 +43,14 @@
           md5 (MessageDigest/getInstance "md5")]
       (.digest md5 ref-bases))))
 
-(defn write-alignments
-  "TODO"
-  [^CRAMWriter wtr alns header]
-  (let [^OutputStream out (.-stream wtr)
-        ds-encodings ds/default-data-series-encodings
-        ds-encoders (ds/build-data-series-encoders ds-encodings)
-        stats (record/encode-slice-records header ds-encoders {} alns)
+(defn- partition-alignments [slices-per-container records-per-slice alns]
+  (->> alns
+       (partition-all records-per-slice)
+       (partition-all slices-per-container)))
+
+(defn- generate-slice [^CRAMWriter wtr counter alns]
+  (let [ds-encoders (ds/build-data-series-encoders ds/default-data-series-encodings)
+        stats (record/encode-slice-records (.-header wtr) ds-encoders {} alns)
         blocks (->> (vals ds-encoders)
                     (mapcat #(%))
                     (keep (fn [{:keys [content-id ^bytes data] :as block}]
@@ -60,31 +61,89 @@
                     (cons {:content-id 0
                            :data (struct/generate-block :raw 5 0 (byte-array 0))}))
         ref-md5 (reference-md5 wtr stats)
-        slice-header (struct/generate-slice-header-block
+        header-block (struct/generate-slice-header-block
                       (assoc stats
-                             :counter 0
+                             :counter counter
                              :embedded-reference -1
                              :reference-md5 ref-md5)
                       blocks)
-        compression-header (struct/generate-compression-header-block
-                            {:RN true, :AP false, :RR true}
-                            {\A {\T 0, \G 1, \C 2, \N 3}
-                             \T {\A 0, \G 1, \C 2, \N 3}
-                             \G {\A 0, \T 1, \C 2, \N 3}
-                             \C {\A 0, \T 1, \G 2, \N 3}
-                             \N {\A 0, \T 1, \G 2, \C 3}}
-                            [[]] ds-encodings {})
-        container-len (->> blocks
-                           (map #(alength ^bytes (:data %)))
-                           (apply +
-                                  (alength compression-header)
-                                  (alength slice-header)))]
-    (struct/encode-container-header out
-                                    (assoc stats
-                                           :length container-len
-                                           :counter 0
-                                           :blocks (+ 2 (count blocks))
-                                           :landmarks [(alength compression-header)]))
-    (.write out compression-header)
-    (.write out slice-header)
-    (run! (fn [{:keys [data]}] (.write out ^bytes data)) blocks)))
+        block-data (mapv :data blocks)]
+    {:header-block header-block
+     :data-blocks block-data
+     :stats stats
+     :counter (+ (long counter) (long (:records stats)))
+     :size (->> block-data
+                (map #(alength ^bytes %))
+                (apply + (alength header-block)))}))
+
+(defn- generate-slices [wtr counter slices]
+  (loop [[slice & more] slices, counter counter, acc []]
+    (if slice
+      (let [slice' (generate-slice wtr counter slice)]
+        (recur more (:counter slice') (conj acc slice')))
+      acc)))
+
+(defn- write-container [^CRAMWriter wtr counter slices]
+  (let [^OutputStream out (.-stream wtr)
+        ds-encodings ds/default-data-series-encodings
+        slices' (generate-slices wtr counter slices)
+
+        compression-header-block
+        (struct/generate-compression-header-block
+         {:RN true, :AP false, :RR true}
+         {\A {\T 0, \G 1, \C 2, \N 3}
+          \T {\A 0, \G 1, \C 2, \N 3}
+          \G {\A 0, \T 1, \C 2, \N 3}
+          \C {\A 0, \T 1, \G 2, \N 3}
+          \N {\A 0, \T 1, \G 2, \C 3}}
+         [[]] ds-encodings {})
+
+        stats (->> slices'
+                   (map :stats)
+                   (reduce (fn [acc stats]
+                             (let [start (min (long (:start acc))
+                                              (long (:start stats)))
+                                   end (max (+ (long (:start acc))
+                                               (long (:span acc)))
+                                            (+ (long (:start stats))
+                                               (long (:span stats))))]
+                               {:ref-seq-id (if (or (= (:ref-seq-id acc) -2)
+                                                    (not= (:ref-seq-id acc)
+                                                          (:ref-seq-id stats)))
+                                              -2
+                                              (:ref-seq-id stats))
+                                :start start
+                                :span (- end start)
+                                :bases (+ (long (:bases acc))
+                                          (long (:bases stats)))
+                                :records (+ (long (:records acc))
+                                            (long (:records stats)))}))))
+        container-len (->> slices'
+                           (map (fn [{:keys [^bytes header-block data-blocks]}]
+                                  (apply + (alength header-block)
+                                         (map #(alength ^bytes %) data-blocks))))
+                           (apply + (alength compression-header-block)))
+        landmarks (->> slices'
+                       (reductions #(+ (long %1) (long (:size %2)))
+                                   (alength compression-header-block))
+                       butlast)
+        container-header (assoc stats
+                                :counter counter
+                                :length container-len
+                                :blocks (->> slices'
+                                             (map (comp inc count :data-blocks))
+                                             (apply + 1))
+                                :landmarks landmarks)
+        counter' (:counter (peek slices'))]
+    (struct/encode-container-header out container-header)
+    (.write out compression-header-block)
+    (run! (fn [{:keys [^bytes header-block data-blocks]}]
+            (.write out header-block)
+            (run! #(.write out ^bytes %) data-blocks))
+          slices')
+    counter'))
+
+(defn write-alignments
+  "TODO"
+  [wtr alns _]
+  (reduce (partial write-container wtr) 0 (partition-alignments 1 10000 alns)))

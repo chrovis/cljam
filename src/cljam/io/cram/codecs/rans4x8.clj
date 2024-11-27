377eee9e3dcba73024fda39ef3c50612f2f8056a
Shogo Ohta <shogo@xcoo.jp>
2024-11-27 14:59:47 +0900

    [ci skip] wip: Implement rANS encoder

--- src/cljam/io/cram/codecs/rans4x8.clj
+++ src/cljam/io/cram/codecs/rans4x8.clj
@@ -1,7 +1,9 @@
 (ns cljam.io.cram.codecs.rans4x8
-  (:require [cljam.io.util.byte-buffer :as bb]
-            [cljam.io.cram.itf8 :as itf8])
-  (:import [java.util Arrays]))
+  (:refer-clojure :exclude [reset!])
+  (:require [cljam.io.cram.itf8 :as itf8]
+            [cljam.io.util.byte-buffer :as bb])
+  (:import [java.nio ByteBuffer]
+           [java.util Arrays]))
 
 (def ^:private byte-array-type (type (byte-array 0)))
 (def ^:private int-array-type (type (int-array 0)))
@@ -135,3 +137,100 @@
     (if (zero? order)
       (decode0 bb n-out)
       (decode1 bb n-out))))
+
+(defn- calculate-frequencies0 ^"[B" [^ByteBuffer bb]
+  (let [size (.remaining bb)
+        freqs (int-array 256)
+        _ (dotimes [_ size]
+            (let [b (long (bb/read-ubyte bb))]
+              (aset freqs b (inc (aget freqs b)))))
+        tr (+ (quot (bit-shift-left 4096 31) size)
+              (quot (bit-shift-left 1 31) size))
+        max-idx (loop [i 0, cur 0, m 0]
+                  (if (< i 256)
+                    (let [f (aget freqs i)]
+                      (if (<= f m)
+                        (recur (inc i) cur m)
+                        (recur (inc i) i f)))
+                    cur))
+        fsum (loop [i 0, fsum 0]
+               (if (< i 256)
+                 (let [f (aget freqs i)]
+                   (if-not (zero? f)
+                     (let [f' (unsigned-bit-shift-right (* f tr) 31)]
+                       (aset freqs f (if (zero? f') 1 f'))
+                       (recur (inc i) (+ fsum f')))
+                     (recur (inc i) fsum)))
+                 (inc fsum)))]
+    (aset freqs max-idx
+          (if (< fsum 4096)
+            (+ (aget freqs max-idx) (- 4096 fsum))
+            (- (aget freqs max-idx) (- fsum 4096))))
+    freqs))
+
+(defn- write-frequencies0 [^ByteBuffer out ^"[I" freqs]
+  (let [start (.position out)]
+    (loop [i 0, rle 0]
+      (when (< i 256)
+        (let [f (aget freqs i)]
+          (when-not (zero? f)
+            (let [rle' (if (zero? rle)
+                         (do (.put out (byte i))
+                             (when (and (> i 0) (not (zero? (aget freqs (dec i)))))
+                               (let [rle' (loop [rle (inc i)]
+                                            (if (and (< rle 256)
+                                                     (not (zero? (aget freqs rle))))
+                                              (recur (inc rle))
+                                              (- rle (inc i))))]
+                                 (.put out (byte rle'))
+                                 rle')))
+                         (dec rle))]
+              (if (< f 128)
+                (.put out (byte f))
+                (do (.put out (bit-or 128 (unsigned-bit-shift-right f 8)))
+                    (.put out (bit-and 0xff f))))
+              (recur (inc i) rle'))))))
+    (.put out 0)
+    (- (.position out) start)))
+
+(defprotocol ISymbolState
+  (reset! [this])
+  (init! [this start freq scale-bits])
+  (update! [^ByteBuffer bb b]))
+
+(deftype SymbolState
+         [^:unsynchronized-mutable ^long max
+          ^:unsynchronized-mutable ^long rcp-freq
+          ^:unsynchronized-mutable ^long bias
+          ^:unsynchronized-mutable ^long cmpl-freq
+          ^:unsynchronized-mutable ^long rcp-shift]
+  ISymbolState
+  (reset! [_]
+    (set! max 0)
+    (set! rcp-freq 0)
+    (set! bias 0)
+    (set! cmpl-freq 0)
+    (set! rcp-shift 0))
+  (init! [_ start freq]
+    (set! max (* (bit-shift-left 1 19) freq))
+    (set! cmpl-freq (- (bit-shift-left 1 12) freq))
+    (if (< freq 2)
+      (do (set! rcp-freq (bit-not 0))
+          (set! rcp-shift 0)
+          (set! bias (dec (+ start (bit-shift-left 1 12)))))
+      (let [shift (loop [shift 0]
+                    (if (< (bit-shift-left 1 shift) freq)
+                      (recur (inc shift))
+                      shift))]
+        (set! rcp-freq (quot (dec (+ (bit-shift-left 1 (+ shift 31)) freq)) freq))
+        (set! rcp-shift (dec shift))
+        (set! bias start)))
+    (set! rcp-shift (+ rcp-shift 32)))
+  (update! [_ bb b]
+    (let [x (loop [i 2, x b]
+              (if (or (zero? i) (< x max))
+                x
+                (do (.put bb (byte (bit-and 0xff x)))
+                    (recur (dec i) (unsigned-bit-shift-right x 8)))))
+          q (unsigned-bit-shift-right (* x (bit-and 0xfffffff rcp-freq)) rcp-shift)]
+      (+ x bias (* q cmpl-freq)))))
